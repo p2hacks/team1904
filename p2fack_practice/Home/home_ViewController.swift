ec4406eee462ee5bad5852fa4878c769178e94c3
山田楓也 <fuya0713@gmail.com>
2019-02-17 04:08:19 +0900

    データの値受け渡し

--- p2fack_practice/Home/home_ViewController.swift
+++ p2fack_practice/Home/home_ViewController.swift
@@ -9,56 +9,65 @@
 import UIKit
 
 
-var taskList = [String]()
-
 class home_ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
-    var taskTime = Date()
-    
+     let ktaskdata = "TASK_DATA"
+
+    var task = [TaskData]()
+  
+    //初期ロード
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        load()
         
         self.tableView.register(UINib(nibName:"TableViewCell",bundle:nil), forCellReuseIdentifier:"TableViewCell")
         
-        if UserDefaults.standard.object(forKey: "TaskList") != nil {
-            taskList = UserDefaults.standard.object(forKey: "TaskList") as! [String]
+        if UserDefaults.standard.object(forKey: "TaskData") != nil {
+            task = UserDefaults.standard.object(forKey: "TaskData") as! [TaskData]
         }
     }
-    
     
     //更新
     override func viewWillAppear(_ animated: Bool) {
         tableView.reloadData()
+        super.viewWillAppear(animated)
     }
     
     //数を指定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return taskList.count
+        return task.count
     }
     
     //セルの内容
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
         // セルに表示する値を設定する
-        cell.taskTime!.text = getNowClockString()
-        cell.taskName!.text = taskList[indexPath.row]
+        let now = Date()
+        let date = task[indexPath.row].day
+        let days = Calendar.current.dateComponents([.day], from: date, to: now).day!
+        let format = "\(days)日前"
+        cell.taskTime!.text = format
+        cell.taskName!.text = task[indexPath.row].name
         return cell
     }
     
-    
-    func getNowClockString() -> String {
-        //   let formatter = DateFormatter()
-        let now = Date()
-        let days = Calendar.current.dateComponents([.day], from: taskTime, to: now).day!
-        // formatter.dateFormat = "dd日前"
-        return String(days)
+    //セルの選択
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let message = "リセットしますか？"
         
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okayAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default,handler: nil)
+        let cancelAction = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel,handler: nil)
+        
+        alert.addAction(okayAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion:nil)
     }
-    
+
     //cell高さ
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
         return 100
@@ -67,8 +76,35 @@ class home_ViewController: UIViewController,UITableViewDelegate, UITableViewData
     //cell削除
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         let index = indexPath.row
-        taskList.remove(at: index)
-        UserDefaults.standard.set(taskList, forKey: "TaskList" )
+        task.remove(at: index)
+        //UserDefaults.standard.set(task, forKey: "TaskData" )
+        save()
         tableView.reloadData()
+    }
+    
+    // 読み込み処理
+    func load() {
+        // Dictionary配列を読み込み
+        let userDefaults = UserDefaults.standard
+        if let loadArray = userDefaults.array(forKey: ktaskdata) as? [[String: Any]] {
+            // Dictionary配列->ToDo配列に変換
+            loadArray.forEach({ item in
+                task.append(TaskData(dictionary: item))
+            })
+        }
+    }
+    //保存
+    func save() {
+        var saveArray = [[String: Any]]()
+        
+        //VoiceRecorded配列->Dictionary配列に変換
+        task.forEach ({ item in
+            saveArray.append(item.dictionaryFromTask())
+        })
+        // Dictionary配列を保存
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(saveArray, forKey: ktaskdata)
+        userDefaults.synchronize()
+        
     }
 }
---
+++ p2fack_practice/src/model/TaskData.swift
@@ -0,0 +1,32 @@
+//
+//  TaskData.swift
+//  p2fack_practice
+//
+//  Created by Fuuya Yamada on 2019/02/16.
+//  Copyright © 2019 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class TaskData: NSObject {
+    var day:Date = Date()
+    var name:String = ""
+    
+    let kday = "DAY"
+    let kname = "NAME"
+
+    init(day: Date, name: String) {
+        self.day = day
+        self.name = name
+    }
+    
+    init(dictionary:[String:Any] ) {
+        day = dictionary[kday] as! Date
+        name = dictionary[kname] as! String
+    }
+    
+    func dictionaryFromTask() -> [String:Any]{
+        return [kday : day, kname : name]
+    }
+    
+}
