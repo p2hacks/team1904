68941d361afe51f1f8ce11c4d9c93e9dafc845fa
上野隆斗 <>
2020-02-29 23:39:16 +0900

    [fix]storyboard整理

---
+++ p2fack_practice/No Touch/Alarm/AlarmViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlarmViewController.swift
+//  p2fack_practice
+//
+//  Created by 上野隆斗 on 2020/02/29.
+//  Copyright © 2020 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
