c6ad218f83bb30e1504893b3ccb6ef9de2cfd224
上野隆斗 <>
2020-03-04 12:21:19 +0900

    [feat]colorファイルを追加してボタンの装飾

--- p2fack_practice/Alarm/AlarmViewController.swift
+++ p2fack_practice/Alarm/AlarmViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class AlarmViewController: UIViewController {
     //インスタンスを生成
     let alarm = Alarm()
-    @IBOutlet weak var sleepTimePicker: UIDatePicker!
+    let voice = voice_ViewController()
+    
+        @IBOutlet weak var sleepTimePicker: UIDatePicker!
+
+    @IBOutlet weak var startBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonSetting()
         //UIDatePickerを.timeモードにする
         sleepTimePicker.datePickerMode = UIDatePicker.Mode.time
         //現在の時間をDatePickerに表示
@@ -21,33 +26,41 @@ class AlarmViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func buttonSetting() {
+        startBtn.backgroundColor = MyColor.mainOrange
+        startBtn.layer.cornerRadius = 25.0
+        startBtn.layer.shadowOffset = CGSize(width: 3, height: 3 )
+        startBtn.layer.shadowOpacity = 0.5
+        startBtn.layer.shadowRadius = 10
+        startBtn.layer.shadowColor = UIColor.gray.cgColor
+    }
     @IBAction func alarmBtnWasPressed(_ sender: UIButton) {
         //AlarmにあるselectedWakeUpTimeにユーザーの入力した日付を代入
         alarm.selectedWakeUpTime = sleepTimePicker.date
         //AlarmのrunTimerを呼ぶ
         alarm.runTimer()
-performSegue(withIdentifier:"toSecond", sender: nil)
+        performSegue(withIdentifier:"toSecond", sender: nil)
     }
     
-
+    
     override func viewDidAppear(_ animated: Bool) {
-         //AlarmでsleepTimerがnilじゃない場合
+        //AlarmでsleepTimerがnilじゃない場合
         if alarm.sleepTimer != nil{
             //再生されているタイマーを止める
             alarm.stopTimer()
         }
     }
-
+    
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
---
+++ p2fack_practice/components/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  p2fack_practice
+//
+//  Created by 上野隆斗 on 2020/03/04.
+//  Copyright © 2020 Fuuya Yamada. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MyColor : UIColor {
+   class var mainOrange: UIColor {
+      return UIColor(red: 239/255, green: 176/255, blue: 84/255, alpha: 1)
+    }
+    class var subBlue: UIColor {
+      return UIColor(red: 36/255, green: 67/255, blue: 172/255, alpha: 0.9)
+    }
+}
